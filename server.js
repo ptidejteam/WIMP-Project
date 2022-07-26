862b0e9b34719bb55e1fbb8d71d79a4effcea9d9
timvtinsa <timothe.verstraete@insa-lyon.fr>
2022-07-26 21:28:01 +0200

    Update profile page & add .env config

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ const express = require('express');
 const mustacheExpress = require('mustache-express');
 const dbjson = require('simple-json-db');
 const request = require('request');
+require('dotenv').config();
+const axios = require('axios');
+const open = require('open');
+const bodyParser = require('body-parser');
+
 
 const app = express();
 app.engine('html', mustacheExpress());
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/pages');
-const port = 3000;
-
-
+const config = process.env
+const port = config.PORT;
 
 const db = new dbjson( __dirname + '/db.json');
 
@@ -34,6 +38,11 @@ app.use(function (req, res, next) {
     next();
 });
 
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 app.use(express.static(__dirname + '/static'));
 // ****
 
@@ -56,8 +65,6 @@ app.get('/api/:pp', async (req, res) => {
 })
 
 
-
-
 app.get('/profile/:id', async (req, res) => {
     var arrayDB = await db.get(req.params.id);
     Object.keys(arrayDB.states).forEach(key => {
@@ -67,16 +74,19 @@ app.get('/profile/:id', async (req, res) => {
     // console.log(arrayDB);
     // console.log(arrayDB.firstName + " " +arrayDB.lastName);
     // console.log(Object.values(arrayDB.states))
-    res.render(__dirname + '/pages/profil.html', {"pp" : arrayDB.pp, "name" : arrayDB.firstName + " " +arrayDB.lastName,"states":Object.values(arrayDB.states), "id":req.params.id})
-    
-    
+    res.render(__dirname + '/pages/profil.html', {
+        "pp" : arrayDB.pp, 
+        "name" : arrayDB.firstName + " " + arrayDB.lastName,
+        "states":Object.values(arrayDB.states),
+        "id":req.params.id
+    });    
 })
 
 
 
 app.get('/', async (req, res) => {
     // -get the json
-    let all_states = await prmsRequest("http://192.168.0.200:8000/states");
+    let all_states = await prmsRequest("http://localhost:8000/states");
     all_states = JSON.parse(all_states);
     console.log(all_states)
     let arrayDB = Object.values(db.JSON());
@@ -102,7 +112,43 @@ app.get('/', async (req, res) => {
         }
     });
     console.log(arrayDB);
-    res.render(__dirname + '/pages/home.html', {"articles":arrayDB})
+    res.render(__dirname + '/pages/home.html', {"articles":arrayDB});
+});
+
+app.get('/red', async (req, res) => {
+    const params = new URLSearchParams();
+    params.append('id', 'Get Fitbit Profile');
+    console.log(params.toString())
+    console.log("http://" + config.NODE_RED_EXPRESS + "/myflow?" + params.toString())
+    axios
+      .get("http://" + config.NODE_RED_EXPRESS + "/myflow?" + params.toString())
+      .then(function (response) {
+          open("http://" + config.NODE_RED_EXPRESS + "/red/#flow/" + response.data.id, function (err) {
+            if ( err ) throw err;    
+          });
+      })
+      .catch(function (error) {
+            console.log(error);
+            res.send("An error has occurred").status(500);
+      });
+    res.send("OK").status(200);
+    return res;
+});
+
+
+app.post('/update-states', async (req, res) => {
+    const id = req.body.id;
+    const states = req.body.states;
+    if (!db.has(id)) {
+        res.send("No user found").status(404);
+    }
+    else {
+        const user = db.get(id);
+        user.states = states;
+        db.set(id, user);
+        res.send("OK").status(200);
+    }
+    return res;
 });
 
 
