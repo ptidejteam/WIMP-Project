4d6dc437e8c02b421fb84bc45b47c8d83fffda90
YahiaFellah <33671463+yahiaelfellah@users.noreply.github.com>
2024-10-20 10:01:43 -0400

    update utils package :  - add hashPassword

--- packages/@wimp-project/utils/lib/utils.js
+++ packages/@wimp-project/utils/lib/utils.js
@@ -1,20 +1,29 @@
 'use strict';
 
-import { readFileSync } from 'fs';
-import { join } from 'path';
+const fs = require('fs');
+const path = require('path');
+const crypto = require("crypto");
 
-export default {
-  readSecurityFile
+module.exports = {
+  readSecurityFile,
+  hashPassword
 }
 
 function readSecurityFile() {
   // Use path.join to create the correct relative path
-  const privateKeyPath = join(__dirname, '../tls/private.key');
+  const privateKeyPath = path.join(__dirname, '../tls/private.key');
   try {
-    return readFileSync(privateKeyPath, { encoding: 'utf-8' });
+    return fs.readFileSync(privateKeyPath, { encoding: 'utf-8' });
   } catch (error) {
     // Handle any errors that might occur during file reading, e.g., the file doesn't exist.
    // console.error('Error reading private.key:', error.message);
     throw Error("Can't read the file private.key" + error.message);
   }
 }
+
+// Helper function to hash passwords (externalize if reusable)
+function hashPassword (password) {
+  const salt = crypto.randomBytes(16).toString("base64");
+  const hash = crypto.scryptSync(password, salt, 64, { N: 16384 }).toString("base64");
+  return `${salt}$${hash}`;
+};
