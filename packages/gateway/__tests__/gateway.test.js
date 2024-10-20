022ccc686fd8a03711fa441643285b160dbedf13
YahiaFellah <33671463+yahiaelfellah@users.noreply.github.com>
2024-10-20 15:55:06 -0400

    delete unused test files

--- packages/gateway/__tests__/gateway.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const gateway = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(gateway(), 'Hello from gateway');
-console.info('gateway tests passed');
