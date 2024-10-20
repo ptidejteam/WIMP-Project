2ff6da4b2e0c94ef8fab82d7e0eab16928d981dd
YahiaFellah <33671463+yahiaelfellah@users.noreply.github.com>
2024-10-20 16:25:26 -0400

    update several files in the project

--- packages/gateway/lib/routes/routes.js
+++ packages/gateway/lib/routes/routes.js
@@ -1,18 +1,14 @@
 const API_PREFIX = "api/v1";
-const USER_URL = process.env.USER_URL || "http://0.0.0.0:3001"; // Replace with your actual environment variable name
-const FLOW_URL = process.env.FLOW_URL || "http://0.0.0.0:3002";
-const DEVICE_URL = process.env.FLOW_URL || "http://0.0.0.0:3003";
-const DEPARTEMENT_URL = process.env.FLOW_URL || "http://0.0.0.0:3004";
-const CAMERA_URL = process.env.CAMERA || "http://0.0.0.0:3006";
-const WEMO_URL = process.env.WEMO || "http://0.0.0.0:3007";
+const USER_URL = process.env.USER_URL;
+const DEVICE_URL = process.env.FLOW_URL;
 exports.routes = [
   {
-    url: `/${API_PREFIX}/auth`,
+    url: `/${API_PREFIX}/login`,
     proxy: {
-      target: `${USER_URL}/auth`,
+      target: `${USER_URL}/login`,
       changeOrigin: true,
       pathRewrite: {
-        [`^/${API_PREFIX}/auth`]: "",
+        [`^/${API_PREFIX}/login`]: "",
       },
     },
   },
@@ -34,16 +30,6 @@ exports.routes = [
     },
   },
   {
-    url: `/${API_PREFIX}/admin`,
-    proxy: {
-      target: `${USER_URL}/admin`,
-      changeOrigin: true,
-      pathRewrite: {
-        [`^/${API_PREFIX}/admin`]: "",
-      },
-    },
-  },
-  {
     url: `/${API_PREFIX}/users`,
     authenticationRequired: true,
     proxy: {
@@ -54,28 +40,6 @@ exports.routes = [
       },
     },
   },
-  // {
-  //   url: `/${API_PREFIX}/departments`,
-  //   authenticationRequired: true,
-  //   proxy: {
-  //     target: `${DEPARTEMENT_URL}/departments`,
-  //     changeOrigin: true,
-  //     pathRewrite: {
-  //       [`^/${API_PREFIX}/departments`]: "",
-  //     },
-  //   },
-  // },
-  // {
-  //   url: `/${API_PREFIX}/flows`,
-  //   authenticationRequired: true,
-  //   proxy: {
-  //     target: `${FLOW_URL}/flows`,
-  //     changeOrigin: true,
-  //     pathRewrite: {
-  //       [`^/${API_PREFIX}/flows`]: "",
-  //     },
-  //   },
-  // },
   {
     url: `/${API_PREFIX}/devices`,
     authenticationRequired: true,
@@ -87,26 +51,5 @@ exports.routes = [
       },
     },
   },
-  // {
-  //   url: `/${API_PREFIX}/camera`,
-  //   authenticationRequired: true,
-  //   isgRPC : false,
-  //   proxy: {
-  //     target: `${CAMERA_URL}/camera`,
-  //     changeOrigin: true,
-  //     pathRewrite: {
-  //       [`^/${API_PREFIX}/camera`]: "",
-  //     },
-  //   },
-  // },
-  // {
-  //   url: `/${API_PREFIX}/wemo`,
-  //   authenticationRequired: true,
-  //   isgRPC : true,
-  //   proxy: {
-  //     target: `${WEMO_URL}`,
-  //     port: 3007
-  //   },
-  // },
   // Add more routes as needed
 ];
