483f6985860f2dece5733f0810bb51c6e78c7c05
YahiaFellah <33671463+yahiaelfellah@users.noreply.github.com>
2024-10-20 15:20:52 -0400

    misc : change the endpoint from auth to login

--- packages/user/lib/security/routes.config.js
+++ packages/user/lib/security/routes.config.js
@@ -3,7 +3,7 @@ const Authenticator = require('./authentication/authentication.handler');
 
 
 exports.routesConfig = function (app) {
-    app.post('/auth', [
+    app.post('/login', [
         IdentityChecker.hasAuthValidFields,
         IdentityChecker.isPasswordAndUserMatch,
         Authenticator.login
