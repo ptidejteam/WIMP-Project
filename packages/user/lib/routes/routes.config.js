97c5479937a2a4779f100903126594ea77084cc4
YahiaFellah <33671463+yahiaelfellah@users.noreply.github.com>
2024-10-27 09:31:16 -0400

    update user routes

--- packages/user/lib/routes/routes.config.js
+++ packages/user/lib/routes/routes.config.js
@@ -3,27 +3,16 @@ const AvailabilityProvider = require("./controllers/availability.provider");
 
 exports.routesConfig = (app) => {
   app.post("/users", [IdentityProvider.insert]);
-
   app.get("/users", [IdentityProvider.list]);
-
   app.get("/users/:userId", [IdentityProvider.getById]);
-
   app.put("/users/:userId", [
-    // IdentityProvider.uploadAvatar, // Upload the avatar first
     IdentityProvider.putById, // Then update the user
   ]);
-
   app.delete("/users/:userId", [IdentityProvider.removeById]);
-
   // Define routes using the UserAvailabilityController
-  app.post("/availability", AvailabilityProvider.insertOrUpdate);
-  app.get("/availability", AvailabilityProvider.list);
-  app.get("/availability/:userId", AvailabilityProvider.getById); // Retrieve user availability by ID
-  app.patch("/availability", AvailabilityProvider.updateAvailability); // Update user online status
-  // app.patch("/availability/tracking", AvailabilityProvider.setTrackingOption); // Update offline mode
-  // app.patch("/availability", AvailabilityProvider.setAvailabilityStatus); // Update availability status
-  // app.patch("/availability/message", AvailabilityProvider.setCustomMessage); // Update custom message
-  // app.patch("/availability/display", AvailabilityProvider.setDisplayOption); // Update display option
-  app.delete("/availability/:userId", AvailabilityProvider.removeById); // Remove availability by ID
-  
+  app.post("/availability", [AvailabilityProvider.insertOrUpdate]);
+  app.get("/availability", [AvailabilityProvider.list]);
+  app.get("/availability/:userId", [AvailabilityProvider.getById]); // Retrieve user availability by ID
+  app.patch("/availability", [AvailabilityProvider.updateAvailability]); // Update user online status
+  app.delete("/availability/:userId", [AvailabilityProvider.removeById]); // Remove availability by ID
 };
